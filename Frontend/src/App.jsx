a57088c55d86b4389d75d17289ac07d121210760
Nevin-Bali100 <opopopopop100000@gmail.com>
2024-12-22 00:10:59 +0530

    new commits

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,19 @@
 import React from "react";
 import Post from "./Component/Post.jsx";
-import Header from "./Component/Header.jsx";
 import { Route, Routes } from "react-router-dom";
 import "./index.css";
+import Layout from "./Layout.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
 
 const App = () => {
   return (
     <Routes>
-      <Route
-        index
-        element={
-          <main>
-            <Header />
-            <Post />
-            <Post />
-            <Post />
-          </main>
-        }
-      />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Post />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Route>
     </Routes>
   );
 };
