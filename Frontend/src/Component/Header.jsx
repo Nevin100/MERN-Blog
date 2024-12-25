16d351613190db375f4c6493c615856f3a5b2e05
Nevin-Bali100 <opopopopop100000@gmail.com>
2024-12-25 12:32:02 +0530

    new commit

--- Frontend/src/Component/Header.jsx
+++ Frontend/src/Component/Header.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
+
 const Header = () => {
+  const [username, setUsername] = useState(null);
+
+  //useEffect hook to fecth details of profile
   useEffect(() => {
     fetch("http://localhost:4000/profile", {
       credentials: "include",
+    }).then((response) => {
+      response.json().then((userInfo) => {
+        setUsername(userInfo.username);
+      });
     });
   }, []);
+
+  const LogOut = () => {
+    fetch("http://localhost:4000/logout", {
+      credentials: "include",
+      method: "POST",
+    });
+  };
+
   return (
     <div>
       <header>
@@ -15,8 +31,18 @@ const Header = () => {
           Blog
         </Link>
         <nav>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
+          {username && (
+            <>
+              <Link to="/create">Create New Post</Link>
+              <a onClick={LogOut}>LogOut</a>
+            </>
+          )}
+          {!username && (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+            </>
+          )}
         </nav>
       </header>
     </div>
