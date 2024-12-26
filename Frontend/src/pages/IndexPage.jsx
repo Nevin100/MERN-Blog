826931cc55c87bced8cad8a74ff764c62b3840fa
Nevin-Bali100 <opopopopop100000@gmail.com>
2024-12-27 01:14:22 +0530

    new commit

--- Frontend/src/pages/IndexPage.jsx
+++ Frontend/src/pages/IndexPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import Post from "../Component/Post";
 
+//2:19:00 start
 const IndexPage = () => {
   useEffect(() => {
     const response = fetch("http://localhost:4000/post").then((response) => {
